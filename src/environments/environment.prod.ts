718a904c9f35f6d26fa1ea0e40e6ced5d0c077db
AnriTapel <anri.snep@gmail.com>
2020-06-14 15:53:38 +0300

    firebase init & firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZY90KvJb_rNe2iTs6MYV-fPdrQfKcYmQ",
+    authDomain: "bizmate-5f9a4.firebaseapp.com",
+    databaseURL: "https://bizmate-5f9a4.firebaseio.com",
+    projectId: "bizmate-5f9a4",
+    storageBucket: "bizmate-5f9a4.appspot.com",
+    messagingSenderId: "995772534663",
+    appId: "1:995772534663:web:d7a509571d774cced30d22",
+    measurementId: "G-4W6MFFE1TX"
+  }
 };
