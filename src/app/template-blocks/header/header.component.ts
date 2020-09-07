86d2bb1a6ba8ba727deb7185cade452c5dc99e79
AnriTapel <anri.snep@gmail.com>
2020-09-07 22:55:24 +0300

    mobile menu smooth appearance

--- src/app/template-blocks/header/header.component.ts
+++ src/app/template-blocks/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {LoginComponent} from "../../dialogs/login/login.component";
 import {MatDialogConfig} from "../../dialogs/mat-dialog-config";
@@ -14,13 +14,13 @@ import {AppService} from "../../services/app/app.service";
 export class HeaderComponent implements OnInit {
 
   public loggedIn: boolean;
-  public isMobileMenuOpened: boolean;
   public userName: string;
 
-  constructor(private dialog: MatDialog, private auth: AuthService, private router: Router) { }
+  constructor(private dialog: MatDialog, private auth: AuthService, private router: Router) {
+  }
 
   ngOnInit(): void {
-    this.isMobileMenuOpened = false;
+    this.hideMobileMenu();
     this.auth.user$.subscribe((res) => {
       this.loggedIn = res && !res.isAnonymous;
       this.userName = this.loggedIn ? res.displayName : undefined;
@@ -37,7 +37,17 @@ export class HeaderComponent implements OnInit {
 
   openLoginDialog(): void {
     this.dialog.open(LoginComponent, MatDialogConfig.narrowDialogWindow);
-    this.isMobileMenuOpened = false;
+    this.hideMobileMenu();
+  }
+
+  showMobileMenu(): void {
+    document.getElementById('mobileMenuWrapper').style.visibility = 'visible';
+    document.getElementById('mobileMenuWrapper').style.opacity = '1';
+  }
+
+  hideMobileMenu(): void {
+    document.getElementById('mobileMenuWrapper').style.visibility = 'hidden';
+    document.getElementById('mobileMenuWrapper').style.opacity = '0';
   }
 
   logOut(): void {
@@ -45,7 +55,7 @@ export class HeaderComponent implements OnInit {
     this.auth.signOut().then(() => {
       this.auth.user = null;
       this.router.navigateByUrl("/");
-      this.isMobileMenuOpened = false;
+      this.hideMobileMenu();
     }).finally(() => AppService.hideOverlay());
   }
 }
