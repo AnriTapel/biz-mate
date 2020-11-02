566d9ad9eb6b9819f6e268835c849498321a912c
AnriTapel <anri.snep@gmail.com>
2020-11-02 18:53:55 +0300

    environment files deleted

--- src/environments/environment.default.ts
+++ src/environments/environment.default.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDZY90KvJb_rNe2iTs6MYV-fPdrQfKcYmQ",
-    authDomain: "bizmate-5f9a4.firebaseapp.com",
-    databaseURL: "https://bizmate-5f9a4.firebaseio.com",
-    projectId: "bizmate-5f9a4",
-    storageBucket: "bizmate-5f9a4.appspot.com",
-    messagingSenderId: "995772534663",
-    appId: "1:995772534663:web:d7a509571d774cced30d22",
-    measurementId: "G-4W6MFFE1TX"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
