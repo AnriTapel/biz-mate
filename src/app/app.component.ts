01b33f9a4dd6af0f46fcfcc38d04493b46c3a76b
AnriTapel <anri.snep@gmail.com>
2020-11-23 14:31:17 +0300

    initial loading spinner created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,18 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  private INITIAL_SPINNER_ELEMENT_ID = 'initial_spinner';
+  private isInitialRouteActivated: boolean = false;
+
   constructor() {
+  }
+
+  public onActivate() {
+    if (this.isInitialRouteActivated) {
+      return;
+    }
+
+    document.getElementById(this.INITIAL_SPINNER_ELEMENT_ID).style.display = 'none';
+    this.isInitialRouteActivated = true;
   }
 }
